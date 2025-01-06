dd3923f74ccbdfa86fa206a7ca7a15f754065b4b
haiyex <an.yex@qq.com>
2025-01-06 23:04:14 +0800

    update addConstant method and pass this method

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,9 +10,13 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
+        int i = 0;
+        while (head.rest != null || i == lst.size() -1) {
             head.first += c;
-            head = head.rest;
+            if (head.rest != null) {
+                head = head.rest;
+            }
+            i++;
         }
     }
 
