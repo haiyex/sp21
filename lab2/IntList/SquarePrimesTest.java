209c5721cb1fd7459df9e4a71255735450bb6673
haiyex <an.yex@qq.com>
2025-01-07 00:04:47 +0800

    squarePrimes pass

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testBySelfWrite() {
+        IntList lst = IntList.of( 17, 18, 19, 20, 21);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("289 -> 18 -> 361 -> 20 -> 21", lst.toString());
+        assertTrue(changed);
+    }
 }
